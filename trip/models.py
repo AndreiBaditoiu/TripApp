a6616738d92d6a7596a2e40a3c726753508f3bb0
Andrei <andrei.baditoiu@gmail.com>
2024-10-25 10:41:32 +0300

    Added: trip_list(.html), trip/migrations/0001_initial.py; modified : config/settings, config/urls, trip/admin, trip/models, trip/urls, trip/views.

--- trip/models.py
+++ trip/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 User = get_user_model()
@@ -17,4 +18,18 @@ class Trip(models.Model):
 
 
 class Note(models.Model):
+    EXCURSIONS = (
+        ("event", "Event"),
+        ("dining", "Dining"),
+        ("experience", "Experience"),
+        ("general", "General"),
+    )
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='notes')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=100, choices= EXCURSIONS)
+    img = models.ImageField(upload_to='notes', blank=True, null=True)
+    rating = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.name} in {self.trip.city}"
--- trip/views.py
+++ trip/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Trip
+
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+def trips_list(request):
+    trips = Trip.objects.all()
+    context = {
+        'trips': trips
+    }
+    return render(request, 'trip_list.html', context)
+
+
