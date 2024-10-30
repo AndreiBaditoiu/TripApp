a1ac9a826315c0a660eda6dd1ffbecfa7748db58
Andrei <andrei.baditoiu@gmail.com>
2024-10-30 12:07:28 +0200

    Added logged_out.html; modified: settings.py, trip/templates/trip_list, trip/templates/trip_list. db.sqlite3.

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-LOGIN_REDIRECT_URL = 'trip-list'
+LOGIN_REDIRECT_URL = reverse_lazy('trips-list')
